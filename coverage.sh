d36e8748e3c4cf1fff388d89f7e3002d45d3397b
valord577 <valord577@gmail.com>
2023-12-21 11:47:43 +0800

    update coverage.sh

--- coverage.sh
+++ coverage.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
-BASE_DIR=$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)
+PROJ_ROOT=$(cd "$(dirname ${BASH_SOURCE[0]})"; pwd)
 
-COVERAGE_DIR="${BASE_DIR}/.coverage"
-OUT_TXT="out.txt"
-OUT_HTML="out.html"
+COVERAGE="${PROJ_ROOT}/.coverage"
 
 # -- 0.
-if [ ! -d "${COVERAGE_DIR}" ]; then
-  mkdir "${COVERAGE_DIR}"
-fi
+if [ ! -d "${COVERAGE}" ]; then (mkdir -p "${COVERAGE}") fi
 # -- 1. coverage.txt
-go test -race -coverprofile="${COVERAGE_DIR}/${OUT_TXT}" -covermode=atomic
+go test -race -coverprofile="${COVERAGE}/coverage.txt" -covermode=atomic ${PROJ_ROOT}/...
 # -- 2. coverage.html
-go tool cover -html "${COVERAGE_DIR}/${OUT_TXT}" -o "${COVERAGE_DIR}/${OUT_HTML}"
+go tool cover -html "${COVERAGE}/coverage.txt" -o "${COVERAGE}/coverage.html"
