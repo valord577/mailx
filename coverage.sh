453dbc4cb117c33e63af27be83c84dbc3ed506f1
valord577 <30427460+valord577@users.noreply.github.com>
2024-01-10 11:39:25 +0800

    update ci: coverage

--- coverage.sh
+++ coverage.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
+set -e
 
 PROJ_ROOT=$(cd "$(dirname ${BASH_SOURCE[0]})"; pwd)
 
-COVERAGE="${PROJ_ROOT}/.coverage"
+export GO111MODULE="on"
+export CGO_ENABLED="1"
+export GOPROXY="https://goproxy.cn,direct"
+export GOSUMDB="sum.golang.google.cn"
 
-# -- 0.
-if [ ! -d "${COVERAGE}" ]; then (mkdir -p "${COVERAGE}") fi
 # -- 1. coverage.txt
-go test -race -coverprofile="${COVERAGE}/coverage.txt" -covermode=atomic ${PROJ_ROOT}/...
+go test -race -coverprofile="${PROJ_ROOT}/coverage.txt" -covermode=atomic ${PROJ_ROOT}/...
 # -- 2. coverage.html
-go tool cover -html "${COVERAGE}/coverage.txt" -o "${COVERAGE}/coverage.html"
+go tool cover -html "${PROJ_ROOT}/coverage.txt" -o "${PROJ_ROOT}/coverage.html"
